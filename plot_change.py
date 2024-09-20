b943a801f8c2934106f6e3335f8b3d667a5923d9
Kaja Louise Havig Bredvold <kajalhb@met.no>
2024-09-20 13:32:12 +0000

    add ensemble statistics and change over models and update periodID values in netcdfs

--- plot_change.py
+++ plot_change.py
@@ -2,74 +2,83 @@ import matplotlib.pyplot as plt
 import xarray as xr
 from pathlib import Path
 
-indicator = "102"
-model = "noresm_hclim"
-cmip5_models = [
-    "cnrm_aladin",
-    "ecearth_cclm",
-    "ecearth_rca",
-    "ecearth_hirham",
-    "hadgem_rca",
-    "hadgem_remo",
-    "mpi_cclm",
-    "mpi_remo",
-    "noresm_rca",
-    "noresm_remo",
-]
-cmip6_models = [
-    "cnrm_hclim",
-    "cnrm_racmo",
-    "ecearth_racmo",
-    "ecearthveg_cclm",
-    "ecearthveg_hclim",
-    "miroc_icon",
-    "mpi_hclim",
-    "mpi_icon",
-    "mpi_racmo",
-    "noresm_hclim",
-]
 
-if model in cmip5_models:
-    scenarios = ["rcp26", "rcp45"]
-elif model in cmip6_models:
-    scenarios = ["ssp370"]
-else:
-    raise KeyError
+def plot_and_save_change(model: str, indicator_id: str):
 
-path_to_netcdfs = Path(
-    f"/lustre/storeC-ext/users/klimakverna/development/Klimakverna-pilot-KAPy/KAPy/results/7.netcdf/{model}"
-)
-path_to_save_figures = Path(
-    f"/lustre/storeC-ext/users/klimakverna/development/Klimakverna-pilot-KAPy/testcase_1_results/{model}"
-)
-files = [
-    # "/lustre/storeC-ext/users/kin2100/MET/annmeans_bc/far_future_mean/pr/30yrmean_ff_ecearth-r12i1p1-cclm_rcp26_3dbc-eqm-sn2018v2005_rawbc_norway_1km_pr_annual_merged.nc"
-    f"{path_to_netcdfs}/{indicator}_{scenario}_change_periods.nc"
-    for scenario in scenarios
-]
+    cmip5_models = [
+        "cnrm_aladin",
+        "ecearth_cclm",
+        "ecearth_hirham",
+        "ecearth_rca",
+        "hadgem_rca",
+        "hadgem_remo",
+        "mpi_cclm",
+        "mpi_remo",
+        "noresm_rca",
+        "noresm_remo",
+    ]
+    cmip6_models = [
+        "cnrm_hclim",
+        "cnrm_racmo",
+        "ecearth_racmo",
+        "ecearthveg_cclm",
+        "ecearthveg_hclim",
+        "miroc_icon",
+        "mpi_hclim",
+        "mpi_icon",
+        "mpi_racmo",
+        "noresm_hclim",
+    ]
 
-cmap = plt.cm.PuOr
-alpha = 0.8
-# fig = plt.figure()
-# ds = xr.open_dataset(files[0])
-# print(ds)
-# ds["pr"].isel(time=0).plot(robust=True, vmin=-1e-5, vmax=1e-5, cmap=cmap, alpha=alpha)
-# plt.savefig(f"{path_to_save_figures}/{indicator}_{scenario[0]}_pr_{model}")
+    if model in cmip5_models:
+        scenarios = ["rcp26", "rcp45"]
+    elif model in cmip6_models:
+        scenarios = ["ssp370"]
+    else:
+        print(f"\n'{model}' is not a valid model name\n")
+        return
 
-for sceanrio_idx, filename in enumerate(files):
-    ds = xr.open_dataset(filename)
-    print(ds)
-    for period in range(0, len(scenarios) + 1):
-        plt.figure()
-        ds["indicator_mean"].isel(periodID=period).plot(robust=True, vmin=-1e-5, vmax=1e-5, cmap=cmap, alpha=alpha)
-        plt.savefig(
-            f"{path_to_save_figures}/{indicator}_{scenarios[sceanrio_idx]}_indicator_mean_{model}_period_{period}"
-        )
+    path_to_netcdfs = Path(
+        f"/lustre/storeC-ext/users/klimakverna/development/Klimakverna-pilot-KAPy/KAPy/results/7.netcdf/{model}"
+    )
+    path_to_save_figures = Path(
+        f"/lustre/storeC-ext/users/klimakverna/development/Klimakverna-pilot-KAPy/testcase_1_results/{model}"
+    )
+    files = [f"{path_to_netcdfs}/{indicator_id}_{scenario}_change_periods.nc" for scenario in scenarios]
 
-        fig = plt.figure()
-        ds["indicator_mean_rel"].isel(periodID=period).plot(robust=True, vmin=-20, vmax=20, cmap=cmap, alpha=alpha)
-        plt.savefig(
-            f"{path_to_save_figures}/{indicator}_{scenarios[sceanrio_idx]}_indicator_mean_rel_{model}_period_{period}"
-        )
+    cmap = plt.cm.PuOr
+    alpha = 0.8
+    plot_variables = ["indicator_mean_change", "indicator_mean_relative_change"]
+    plot_limits = [[-1e-5, 1e-5], [-20, 20]]
+    periods = [2, 3]
 
-plt.show()
+    for sceanrio_idx, filename in enumerate(files):
+        try:
+            ds = xr.open_dataset(filename)
+            print(ds)
+        except FileNotFoundError:
+            print(f"\nCan't find netcdf file '{filename}'\n")
+            return
+
+        for period in periods:
+            for variable, limit in zip(plot_variables, plot_limits):
+                plt.figure()
+                ds[variable].sel(periodID=period).plot(
+                    robust=True, vmin=limit[0], vmax=limit[-1], cmap=cmap, alpha=alpha
+                )
+                try:
+                    plt.savefig(
+                        f"{path_to_save_figures}/{indicator_id}_{scenarios[sceanrio_idx]}_{variable}_{model}_period_{period}"
+                    )
+                except FileNotFoundError:
+                    print(
+                        f"\nDidn't manage to save plot for model '{model}', indicator '{indicator_id}', "
+                        f"scenario '{scenarios[sceanrio_idx]}', variable '{variable}' and period '{period}'. "
+                        f"\nCheck that '{path_to_save_figures}' exists\n"
+                    )
+
+
+if __name__ == "__main__":
+    model = "hadgem_remo"
+    indicator = "102"
+    plot_and_save_change(model, indicator)
